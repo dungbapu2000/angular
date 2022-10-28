cecb5ff3ad2ae85601f3c0151060e69eb55c70b9
dungbapu2000 <81693998+dungbapu2000@users.noreply.github.com>
2022-10-28 20:03:44 +0700

    chat

--- src/app/component/chat/chat.component.ts
+++ src/app/component/chat/chat.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { AuthService } from 'src/app/shared/auth.service';
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.component.html',
@@ -7,9 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChatComponent implements OnInit {
 
-  constructor() { }
+  constructor(private auth: AuthService) { }
 
   ngOnInit(): void {
   }
-
+  register() {
+    this.auth.logout();
+  }
 }
